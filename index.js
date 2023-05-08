0bcdb2685cdbeef30e170252ef79976868989c02
sruthi272000 <127134827+sruthi272000@users.noreply.github.com>
2023-05-08 07:41:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function increament()
+{
+ var number=Number(document.getElementById('number').innerHTML)
+ document.getElementById('number').innerHTML=number+1
+}
+function decreament()
+{
+ var number=Number(document.getElementById('number').innerHTML)
+ document.getElementById('number').innerHTML=number-1
+}
+function neutral()
+{
+    var number=Number(document.getElementById('number').innerHTML)
+    document.getElementById('number').innerHTML=0
+}
